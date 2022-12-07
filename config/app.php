7be2352f327623a5ef237b54180f33d424592587
Ilyas Syaf <ilyas.syafii@gmail.com>
2022-12-07 23:59:15 +0700

    datatable plugin

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,6 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
