f51e5f5fa6f76764d85631464d05c88ac0adf56d
ilyassyaf <ilyas.syafii@gmail.com>
2023-02-22 10:48:47 +0700

    fix menu icon

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -51,7 +51,7 @@
             <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <div class="parent-icon"><i class="bx bx-exit"></i>
                 </div>
-                {{ __('Logout') }}
+                <div class="menu-title">{{ __('Logout') }}</div>
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
